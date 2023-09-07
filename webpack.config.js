31f9551b716cbae91db5aec646cede612f0dddee
Nathan Jones <nathanjones@pop-os.localdomain>
2023-09-07 11:18:20 -0500

    intial project setup and build

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
 
- const path = require(path);
- const HtmlWebpackPlugin = require(html-webpack-plugin);
- const webpack = require("webpack");
-
- module.exports = {
-   entry: ./src/index.js,
-   devtool: inline-source-map,
-   devServer: {
-      static: ./dist,
-      hot: true,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: Hot Module Replacement,
-      }),
-    ],
-   output: {
-     filename: bundle.js,
-     path: path.resolve(__dirname, dist),
-     clean: true,
-   },
-   optimization: {
-    runtimeChunk: single,
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js',
   },
-   module: {
-     rules: [
-       {
-         test: /\.css$/i,
-         use: [style-loader, css-loader],
-       },
-       {
-         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-         type: asset/resource,
-       },
-       {
-         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-         type: asset/resource,
-       },
-
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To-Do List',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
       {
-
-        test: /\.(csv|tsv)$/i,
-
-        use: [csv-loader],
-
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
-
       {
-
-        test: /\.xml$/i,
-
-        use: [xml-loader],
-
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
-     ],
-   },
- };
-
+    ],
+  },
+};
