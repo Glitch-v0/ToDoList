41b8bee3400c6c3c450fc5165e91b4d15e3c2239
Nathan Jones <nathanjones@pop-os.localdomain>
2023-09-25 11:44:01 -0500

    add load functionality, save icon to icons

--- src/display.js
+++ src/display.js
@@ -1,8 +1,8 @@
 import * as ec from "./elementCreation.js"
-import addDemoProjects from "./demoProjects.js";
+import * as storage from "./storage.js";
 
 export function initDisplay(){
-    let projects = addDemoProjects();
+    let projects = storage.load();
     ec.createProjectsAndItemContainers();
     let sorted_projects = Object.keys(projects).sort();
     ec.newProjectIcon(projects, sorted_projects);
---
+++ src/storage.js
@@ -0,0 +1,34 @@
+import addDemoProjects from "./demoProjects.js";
+
+export function load(){
+    let projects;
+    if (isLocalStorageAvailable()) {
+        if (localStorage.getItem('projects') !== null) {
+            console.log('Projects are stored!');
+            projects = localStorage.getItem('projects');
+        } else {
+            console.log('No projects are stored!');
+            projects = addDemoProjects();
+        }
+    } else {
+        console.log("Local storage is not available!");
+    }
+    return projects
+};
+
+
+export function save(projects){
+    localStorage.setItem('projects', projects);
+};
+
+export function isLocalStorageAvailable () {
+  var test = 'test';
+  console.log('Checking if storage is available...')
+  try {
+    localStorage.setItem (test, test);
+    localStorage.removeItem (test);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
